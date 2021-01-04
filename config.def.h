b890f74be38cd1ad3f33a96bae6336b65cef4d4a
Folkert Bleichrodt <folkert.bleichrodt@applusglobal.com>
2021-01-04 11:27:47 +0100

    Change to iosevka

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:size=13:antialias=true:autohint=true";
+static char *font = "Iosevka:size=13:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = { "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 
