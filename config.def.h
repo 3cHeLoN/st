a375cac802694de0176bba53f39dd06503cc5cb0
Folkert Bleichrodt <folkert.bleichrodt@applusglobal.com>
2023-03-21 09:48:41 +0100

    New font new themes.

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:size=12:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+static char *font = "Fira Code:style=Medium,Regular:size=12:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:size=12:antialias=true:autohint=true" };
 
 static int borderpx = 2;
 
@@ -96,10 +96,10 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 1.0; //0.95;
+float alpha = 1;
 
 // paper color light theme
-static const char *colorname_paper_light[] = {
+static const char *colorname[] = {
     "#eeeeee",
     "#af0000",
     "#008700",
@@ -118,36 +118,10 @@ static const char *colorname_paper_light[] = {
     "#005faf",
     "#005f87",
     [255] = 0,
-    "#eeeeee",
+    //"#eeeeee",
     "#4d4d4d",
 };
 
-/* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-    /* 8 normal colors */
-    [0] = "#000000", /* black   */
-    [1] = "#ff5555", /* red     */
-    [2] = "#50fa7b", /* green   */
-    [3] = "#f1fa8c", /* yellow  */
-    [4] = "#bd93f9", /* blue    */
-    [5] = "#ff79c6", /* magenta */
-    [6] = "#8be9fd", /* cyan    */
-    [7] = "#bbbbbb", /* white   */
-
-    /* 8 bright colors */
-    [8]  = "#44475a", /* black   */
-    [9]  = "#ff5555", /* red     */
-    [10] = "#50fa7b", /* green   */
-    [11] = "#f1fa8c", /* yellow  */
-    [12] = "#bd93f9", /* blue    */
-    [13] = "#ff79c6", /* magenta */
-    [14] = "#8be9fd", /* cyan    */
-    [15] = "#ffffff", /* white   */
- 
-    /* special colors */
-    [256] = "#282a36", /* background */
-    [257] = "#f8f8f2", /* foreground */
-};
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
